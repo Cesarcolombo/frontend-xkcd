293c5b2145c49fbef823013562cd9e9a7126f6ce
César Colombo <cesarcolombo3@gmail.Com>
2026-03-10 16:12:36 +0100

    fin de Step 1

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ Intro to Javascript networking and asynchronous programming
 2. Open index.html in a browser
 3. Follow the instructions in that file
 4. Have fun
+
+
+## Step 0
+img field propably allow to put an image on the page that also comme from the website. num is probably a number for referencing the page
+
+the content looks like the code of a file. some kind of code that make it visible on a browser
+
+## Step 1
+We create a js function to fecth a page and print one of its data
+
+## Step 2
+We upgrade the visualisation of the page. To ensure image and button do not everlap eachtoher
+
--- code.js
+++ code.js
@@ -1,3 +1,29 @@
 const XKCD = "https://xkcd.now.sh/?comic="
 
 // write your code here
+const statusbar = document.getElementById("num");
+const resetBtn = document.getElementById("reset");
+const imgxkcd = document.getElementById("imgxkcd");
+
+
+const fetchIssue = (num) => {
+    let url = XKCD +num;
+    console.log(`request to ${url}`);
+
+    let promesse = fetch(url);
+
+    promesse.then((reponse) =>{
+        return reponse.json();
+    })
+    
+    .then((json) => {
+        let num = json.num;
+        console.log(`num = ${num}`);
+        imgxkcd.src = json.img
+        statusbar.innerText = `Current number ${num}`
+    })
+}
+
+
+resetBtn.addEventListener("click", ()=> fetchIssue("latest"));
+
